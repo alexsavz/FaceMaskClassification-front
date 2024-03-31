08097844c487b3e9a1bd8f141951b46f749993e7
alexsavz <asavina13430@gmail.com>
2024-03-31 18:04:15 +0200

    api cors fix

--- src/app/page.js
+++ src/app/page.js
@@ -12,6 +12,7 @@ export default function Home() {
   const [filename, setFilename] = useState("");
   const [content_type, setContent_type] = useState("");
   const [prediction, setPrediction] = useState("");
+  const API = `${process.env.NEXT_PUBLIC_AWS_API}/api/predict`;
 
   // formulaire POST utilisant l'API AWS
   const handleSubmit = async (event) => {
@@ -22,15 +23,9 @@ export default function Home() {
       formData.append("file", image);
       formData.append("filename", filename);
       formData.append("content_type", content_type);
-      const response = await axios.post(
-        `${process.env.AWS_API}/api/predict`,
-        formData
-      );
-      if (response.data) {
-        console.log(response.data);
-      } else {
-        alert("Une erreur est survenue");
-      }
+      await axios.post(API, formData).then((response) => {
+        setPrediction(response.data);
+      });
     } catch (error) {
       alert(error.message);
     }
@@ -46,34 +41,40 @@ export default function Home() {
         Postez l&apos;image d&apos;un visage pour obtenir une prédiction
         réalisée par un modèle de deep-learning.
       </p>
-      <div className="max-w-sm mx-auto my-8 bg-white shadow py-8 px-8 rounded-xl">
+      <div className="max-w-3xl mx-auto my-8 bg-white shadow py-8 px-8 rounded-xl">
         <h2 className="text-base font-semibold text-slate-700">
           &Eacute;tapes préalables à l&apos;inférence :
         </h2>
         <ul className="mt-3 list-disc pl-5 space-y-3 text-slate-500 marker:text-sky-400">
-          <li>5 cups chopped Porcini mushrooms</li>
-          <li>5 cups chopped Porcini mushrooms</li>
-          <li>5 cups chopped Porcini mushrooms</li>
+          <li>Développement d&apos;une API pour intéragir avec le modèle</li>
+          <li>L&apos;application est conteneurisée avec Docker</li>
+          <li>L&apos;image Docker est déployée sur AWS Fargate</li>
+          <li>Nous envoyons une requête à cette API</li>
+          <li>La requête est transmise au modèle pour l&apos;inférence</li>
+          <li>La prédiction est envoyée au client, ici</li>
         </ul>
       </div>
-
-      <Image
+      <p className="mt-3 text-2xl">
+        Le modèle est entraîné sur 308 images et réalise 93% de bonnes
+        prédictions sur un échantillon de test de 44 images.
+      </p>
+      {/* <Image
         src="/votre-image-d-exemple.jpg" // Mettez le chemin de votre image d'échantillon
         alt="Échantillon"
         width={500}
         height={300}
         className="my-5"
-      />
+      /> */}
 
       {/* Formulaire POST avec l'image sélectionnée */}
       <form
         onSubmit={handleSubmit}
-        className="flex flex-col items-center space-x-6"
+        className="flex flex-col items-center space-x-6 my-4"
       >
         <div className="flex flex-row items-center">
           <div className="shrink-0 px-4 py-2">
             <Image
-              className="h-16 w-16 object-cover rounded-full"
+              className="h-24 w-24 object-cover rounded-full"
               src={imageUrl}
               alt="Current profile photo"
               width={500}
